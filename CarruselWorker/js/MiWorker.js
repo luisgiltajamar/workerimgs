3e4661a42013f580101588607d4836ad5cb99b7c
luisgiltajamar <luis.gil@tajamar365.com>
2014-10-13 12:13:54 +0200

    Con imagenes y parar en el js

---
+++ CarruselWorker/js/MiWorker.js
@@ -0,0 +1,13 @@
+﻿var i = 0;
+
+function cambio() {
+    if (++i >= 5)
+        i = 1;
+
+    postMessage(i + ".jpg");
+
+    setTimeout("cambio()", 3000);
+
+}
+
+cambio();
---
+++ CarruselWorker/js/lanzador.js
@@ -0,0 +1,39 @@
+﻿var trabajador;
+
+function pintar(evt) {
+    document.getElementById("imagen").src = "imgs/" + evt.data;
+}
+
+function error(err) {
+
+    alert(err.message);
+
+}
+
+function parar() {
+    if (trabajador != undefined) {
+        trabajador.terminate();
+        trabajador = undefined;
+    }
+
+
+}
+
+function lanzar() {
+    if (typeof (Worker) !== "undefined" &&
+        trabajador == undefined) {
+
+        trabajador = new Worker("js/MiWorker.js");
+        trabajador.onmessage = pintar;
+        trabajador.onerror = error;
+
+    }
+
+
+}
+
+(function() {
+    document.getElementById("btnStart").addEventListener("click", lanzar);
+    document.getElementById("btnStop").addEventListener("click", parar);
+
+})();
